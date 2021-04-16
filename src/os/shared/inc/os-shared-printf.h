7194f1d0659d1bb17c5654f4e73bd220c45b4563
Joseph Hickey <joseph.p.hickey@nasa.gov>
2021-04-15 23:37:48 -0400

    Fix #832, improve scriptability of shared layer APIs
    To imporove scriptability of generating the shared layer stubs, makes
    two minor adjustments:
    
    - adds a typedef for the callback in OS_ObjectIdIteratorProcessEntry.  This
      avoids the complex syntax of function pointer argument, which is hard to
      read but also harder to parse in a script, too.
    - Splits the "os-shared-printf.h" header into two parts, adding a new
      header file "os-shared-console.h".  This is because OS_ConsoleOutput_Impl
      is implemented in a separate unit in the source, and this allows a better
      relationship between stub files and header files.

--- src/os/shared/inc/os-shared-printf.h
+++ src/os/shared/inc/os-shared-printf.h
@@ -29,52 +29,12 @@
 #define OS_SHARED_PRINTF_H
 
 #include "osapi-printf.h"
+#include "os-shared-console.h"
 #include "os-shared-globaldefs.h"
-
-/*
- * Variables related to the console buffer.
- * This is a simple ring buffer that decouples
- * the OS_printf() call from actual console output.
- *
- * The implementation layer may optionally spawn a
- * "utility task" or equivalent to forward data, or
- * it may process data immediately.
- */
-
-typedef struct
-{
-    char device_name[OS_MAX_API_NAME];
-
-    char *          BufBase;        /**< Start of the buffer memory */
-    size_t          BufSize;        /**< Total size of the buffer */
-    volatile size_t ReadPos;        /**< Offset of next byte to read */
-    volatile size_t WritePos;       /**< Offset of next byte to write */
-    uint32          OverflowEvents; /**< Number of lines dropped due to overflow */
-
-} OS_console_internal_record_t;
-
-extern OS_console_internal_record_t OS_console_table[OS_MAX_CONSOLES];
 
 /****************************************************************************************
                  CONSOLE / DEBUG API LOW-LEVEL IMPLEMENTATION FUNCTIONS
  ****************************************************************************************/
-
-/*---------------------------------------------------------------------------------------
-   Name: OS_ConsoleAPI_Init
-
-   Purpose: Initialize the OS-independent layer for console service
-
-   returns: OS_SUCCESS on success, or relevant error code
----------------------------------------------------------------------------------------*/
-int32 OS_ConsoleAPI_Init(void);
-
-/*----------------------------------------------------------------
-   Function: OS_ConsoleCreate_Impl
-
-    Purpose: Prepare a console device for use
-             For Async devices, this sets up the background writer task
- ------------------------------------------------------------------*/
-int32 OS_ConsoleCreate_Impl(const OS_object_token_t *token);
 
 /*----------------------------------------------------------------
    Function: OS_ConsoleOutput_Impl
@@ -87,20 +47,5 @@ int32 OS_ConsoleCreate_Impl(const OS_object_token_t *token);
    The data is already formatted, this just writes the characters.
  ------------------------------------------------------------------*/
 void OS_ConsoleOutput_Impl(const OS_object_token_t *token);
-
-/*----------------------------------------------------------------
-   Function: OS_ConsoleOutput_Impl
-
-    Purpose: Console output data notification
-
-   This is a notification API that is invoked whenever there
-   is new data available in the console output buffer.
-
-   For a synchronous console service, this may call
-   OS_ConsoleWrite_Impl() directly.  For an async console
-   service, this should wakeup the actual console servicing
-   thread.
- ------------------------------------------------------------------*/
-void OS_ConsoleWakeup_Impl(const OS_object_token_t *token);
 
 #endif /* OS_SHARED_PRINTF_H */
