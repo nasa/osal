cf131cfd7969b977b63940c8826fc2d1182a300f
Alex Campbell <alex.campbell@nasa.gov>
2021-04-16 09:55:45 -0400

    format fix

--- src/tests/file-sys-add-fixed-map-api-test/file-sys-add-fixed-map-api-test.c
+++ src/tests/file-sys-add-fixed-map-api-test/file-sys-add-fixed-map-api-test.c
@@ -63,8 +63,8 @@ void TestFileSysAddFixedMapApi(void)
     UtAssert_True(actual == expected, "OS_TranslatePath() (%ld) == OS_SUCCESS", (long)actual);
 
     /* Test for invalid inputs */
-    //expected = OS_ERR_NAME_TAKEN;
-    actual   = OS_FileSysAddFixedMap(NULL, "./test", "/test");
+    // expected = OS_ERR_NAME_TAKEN;
+    actual = OS_FileSysAddFixedMap(NULL, "./test", "/test");
     UtAssert_True(actual == expected, "OS_FileSysAddFixedMap() (%ld) == OS_INVALID_POINTER", (long)actual);
 
     expected = OS_INVALID_POINTER;
