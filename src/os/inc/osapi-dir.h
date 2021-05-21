92e56e6a6776535ecc490eab6b6237fdc84b054b
Joseph Hickey <joseph.p.hickey@nasa.gov>
2021-05-21 17:42:40 -0400

    Fix #1012, resolve discrepancies between dir API and unit tests
    Ensures correlation between the unit-tests and documented return
    values for the OSAL directory API.

--- src/os/inc/osapi-dir.h
+++ src/os/inc/osapi-dir.h
@@ -53,11 +53,15 @@ typedef struct
  *
  * Prepares for reading the files within a directory
  *
- * @param[out] dir_id    The non-zero handle ID of the directory
- * @param[in]  path      The directory to open
+ * @param[out] dir_id    Location to store handle ID of the directory @nonnull
+ * @param[in]  path      The directory to open @nonnull
  *
  * @return Execution status, see @ref OSReturnCodes
+ * @retval #OS_SUCCESS @copybrief OS_SUCCESS
  * @retval #OS_INVALID_POINTER if dir_id or path is NULL
+ * @retval #OS_FS_ERR_PATH_TOO_LONG if the path argument exceeds the maximum length
+ * @retval #OS_FS_ERR_PATH_INVALID if the path argument is not valid
+ * @retval #OS_ERROR if the directory could not be opened
  */
 int32 OS_DirectoryOpen(osal_id_t *dir_id, const char *path);
 
@@ -70,6 +74,8 @@ int32 OS_DirectoryOpen(osal_id_t *dir_id, const char *path);
  * @param[in] dir_id    The handle ID of the directory
  *
  * @return Execution status, see @ref OSReturnCodes
+ * @retval #OS_SUCCESS @copybrief OS_SUCCESS
+ * @retval #OS_ERR_INVALID_ID if the directory handle is invalid
  */
 int32 OS_DirectoryClose(osal_id_t dir_id);
 
@@ -82,6 +88,8 @@ int32 OS_DirectoryClose(osal_id_t dir_id);
  * @param[in] dir_id    The handle ID of the directory
  *
  * @return Execution status, see @ref OSReturnCodes
+ * @retval #OS_SUCCESS @copybrief OS_SUCCESS
+ * @retval #OS_ERR_INVALID_ID if the directory handle is invalid
  */
 int32 OS_DirectoryRewind(osal_id_t dir_id);
 
@@ -92,10 +100,13 @@ int32 OS_DirectoryRewind(osal_id_t dir_id);
  * Obtains directory entry data for the next file from an open directory
  *
  * @param[in]  dir_id    The handle ID of the directory
- * @param[out] dirent    Buffer to store directory entry information
+ * @param[out] dirent    Buffer to store directory entry information @nonnull
  *
  * @return Execution status, see @ref OSReturnCodes
+ * @retval #OS_SUCCESS @copybrief OS_SUCCESS
  * @retval #OS_INVALID_POINTER if dirent argument is NULL
+ * @retval #OS_ERR_INVALID_ID if the directory handle is invalid
+ * @retval #OS_ERROR at the end of the directory or if the OS call otherwise fails
  */
 int32 OS_DirectoryRead(osal_id_t dir_id, os_dirent_t *dirent);
 
@@ -105,7 +116,7 @@ int32 OS_DirectoryRead(osal_id_t dir_id, os_dirent_t *dirent);
  *
  * Makes a directory specified by path.
  *
- * @param[in]  path      The new directory name
+ * @param[in]  path      The new directory name @nonnull
  * @param[in]  access    The permissions for the directory (reserved for future use)
  *
  * @note Current implementations do not utilize the "access" parameter.  Applications
@@ -117,7 +128,7 @@ int32 OS_DirectoryRead(osal_id_t dir_id, os_dirent_t *dirent);
  * @retval #OS_INVALID_POINTER if path is NULL
  * @retval #OS_FS_ERR_PATH_TOO_LONG if the path is too long to be stored locally
  * @retval #OS_FS_ERR_PATH_INVALID if path cannot be parsed
- * @retval #OS_ERROR if the OS call fails
+ * @retval #OS_ERROR if the OS call fails @covtest
  */
 int32 OS_mkdir(const char *path, uint32 access);
 
@@ -135,7 +146,7 @@ int32 OS_mkdir(const char *path, uint32 access);
  * @retval #OS_INVALID_POINTER if path is NULL
  * @retval #OS_FS_ERR_PATH_INVALID if path cannot be parsed
  * @retval #OS_FS_ERR_PATH_TOO_LONG
- * @retval #OS_ERROR if the directory remove operation failed
+ * @retval #OS_ERROR if the directory remove operation failed @covtest
  */
 int32 OS_rmdir(const char *path);
 /**@}*/
