5f1dbe963e5de04dfc5b290758adc92f92108393
Jacob Hageman <jacob.hageman@nasa.gov>
2022-03-23 09:48:08 -0600

    Fix #1235, Remove old copyright headers

--- ut_assert/scripts/generate_stubs.pl
+++ ut_assert/scripts/generate_stubs.pl
@@ -1,17 +1,15 @@
 #!/usr/bin/perl
 
 #
-#  NASA Docket No. GSC-18,370-1, and identified as "Operating System Abstraction Layer"
+#  NASA Docket No. GSC-18,719-1, and identified as “core Flight System: Bootes”
 #
-#  Copyright (c) 2019 United States Government as represented by
-#  the Administrator of the National Aeronautics and Space Administration.
+#  Copyright (c) 2020 United States Government as represented by the
+#  Administrator of the National Aeronautics and Space Administration.
 #  All Rights Reserved.
 #
-#  Licensed under the Apache License, Version 2.0 (the "License");
-#  you may not use this file except in compliance with the License.
-#  You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
+#  Licensed under the Apache License, Version 2.0 (the "License"); you may
+#  not use this file except in compliance with the License. You may obtain
+#  a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
 #
 #  Unless required by applicable law or agreed to in writing, software
 #  distributed under the License is distributed on an "AS IS" BASIS,
